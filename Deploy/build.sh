02c2d5ff17e9e2e31535c007cf5743d76e93d24d
Oleksandr <sorokin.biochemistry@gmail.com>
2020-12-09 18:25:24 +0100

    deploy update

--- Deploy/build.sh
+++ Deploy/build.sh
@@ -5,5 +5,6 @@ cd ..
 dotnet build WeatherAPI.sln -c Release -o "src/"
 echo "WeatherAPI has been built."
 echo "Building container..."
+cd Deploy
 docker build Dockerfile -t api
 echo "Container has been built."
